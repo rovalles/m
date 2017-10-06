02f6d3bd3b13641966bba084f2bed99e8b02bd68
Rodolfo Ovalles <rovalles0100@gmail.com>
2017-10-06 00:22:22 -0500

    some files

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>M</title>
-
-  <!--link rel="stylesheet" href="css/styles.css?v=1.0"-->
-</head>
-
-    <body>
-        <ul>
+  <head>
+    <meta charset="utf-8">
+    <title>M</title>
+    <link rel="stylesheet" href="assets/main.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <ul>
         <?php
-            $json = file_get_contents('./data.json');
-            $json = json_decode($json, true);
-            foreach( $json as $field => $value ){
-                echo "<li><a href='$value' target='_blank'>$field</a></li>";
-            }
+          $json = file_get_contents('./data.json');
+          $json = json_decode($json, true);
+          foreach( $json as $field => $value ){
+            echo "<li><a href='$value' target='_blank'>$field</a></li>";
+          }
         ?>
-        </ul>
-
-      <!--script src="js/scripts.js"></script-->
-    </body>
+      </ul>
+    </div>
+    <script src="assets/main.js"></script>
+  </body>
 </html>
