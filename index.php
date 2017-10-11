a57fe152053b9b70a1182b3d8111d3e897b272b6
Rodolfo Ovalles <rovalles0100@gmail.com>
2017-10-10 21:27:02 -0700

    organize output and add backgrounds

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
   <head>
     <meta charset="utf-8">
     <title>M</title>
+    <?php
+      $json = file_get_contents('./data.json');
+      $json = json_decode($json, true);
+      $list = "";
+      $bg = "";
+      $count = 1;
+      $wallpaper = "";
+      foreach( $json as $field => $value ){
+        $list .= "<li><a href='$value' target='_blank'>$field</a></li>";
+        $wallpaper .= ".wallpaper-$count{ background-image: url('/assets/images/$count.jpg')!important;  }\n";
+        $count++;
+      }
+
+      echo "<style>$wallpaper</style>";
+    ?>
     <link href="https://fonts.googleapis.com/css?family=Kanit:800" rel="stylesheet">
     <link rel="stylesheet" href="/node_modules/normalize.css/normalize.css">
     <link rel="stylesheet" href="assets/main.css">
@@ -11,13 +26,7 @@
   <body>
     <div class="wrapper">
       <ul>
-        <?php
-          $json = file_get_contents('./data.json');
-          $json = json_decode($json, true);
-          foreach( $json as $field => $value ){
-            echo "<li><a href='$value' target='_blank'>$field</a></li>";
-          }
-        ?>
+        <? echo $list; ?>
       </ul>
     </div>
     <script src="assets/main.js"></script>
